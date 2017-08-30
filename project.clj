9f37b3a5252433e829b92515ba139acd4f0a1935
Val Saven <val.saven@gmail.com>
2017-08-30 11:24:45 +0300

    Initial files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject :4clojure "0.1.0-SNAPSHOT"
+    :description "4clojure"
+    :url "https://github.com/valsaven/4clojure"
+    :license {:name "MIT License"
+              :url "https://opensource.org/licenses/MIT"}
+    :dependencies [[org.clojure/clojure "1.8.0"]])
