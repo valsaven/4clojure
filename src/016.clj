2aa7b6ae067dee35b199e5a7be00a1cbf5c5942c
Val Saven <val.saven@gmail.com>
2017-09-05 21:53:21 +0300

    Add 016-018

---
+++ src/016.clj
@@ -0,0 +1,5 @@
+(=
+ (= ((fn [x] (str "Hello, " x "!")) "Dave") "Hello, Dave!")
+ (= ((fn [x] (str "Hello, " x "!")) "Jenn") "Hello, Jenn!")
+ (= ((fn [x] (str "Hello, " x "!")) "Rhea") "Hello, Rhea!")
+ true)
